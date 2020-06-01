79e03837114380fa84b2c5909725cb7176c185df
Bankole Ahmed <kidrolex19@gmail.com>
2020-06-01 02:17:33 +0100

    create user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,17 +1,30 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const userSchema = new Schema ({
-    firstName : {
+const userSchema = new Schema({
+    firstName: {
         type: String,
         required: true
     },
-        lastName: {
-            type: String,
-            required: true
-        },
-            firstName: {
-                type: String,
-                required: true
-            }
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    confirmPassword: {
+        type: String,
+        required: true
+    },
+    bio: {
+        type: String,
+    }
 })
+
+module.exports = User =mongoose.model('users', userSchema)
